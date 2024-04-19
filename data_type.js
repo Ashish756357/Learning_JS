8b509dfa68a54aa0ce1d3e006405bb7e7d2d9387
Ashish Raj <126260804+Ashish756357@users.noreply.github.com>
2024-04-19 22:13:39 +0530

    Create data_type.js

---
+++ data_type.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// alert(3 + 3) we are using nodejs not browser
+
+
+// Types of data type in JS
+// 1) Number => 2 to power 53
+// 2) int........
+// 3) string  => " "
+// 4) boolen => true/false
+// 5) null  => It is a datatype but also a stand alone value
+      // null is a object type 
+// 6) undefined => The value is not defined
+      // undefined is a undedined type
+// 7) Symbol => unique
+// 8) Object 
+
+console.log(typeof "Ashish")
