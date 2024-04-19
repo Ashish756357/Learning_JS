60e61561bc7ac79b564241bcc36c2eea7228843c
Ashish Raj <126260804+Ashish756357@users.noreply.github.com>
2024-04-19 23:56:17 +0530

    Create conversionOperation.js

---
+++ conversionOperation.js
@@ -0,0 +1,45 @@
+let score = "33"
+console.log(typeof score)
+console.log(score)
+
+// If we try will "33" then we will get the value number still after being in string type
+
+let score = "33adh"
+console.log(typeof score)
+console.log(score)
+
+// same as above we will get the value of score which is 33adh
+
+// NOW THE CONVERSION
+
+let score = 33
+let valueinnumber = Number(score)
+console.log(typeof (valueinnumber));
+console.log(valueinnumber)
+Output =  33
+
+let score = "33"
+let valueinnumber = Number(score)
+console.log(typeof (valueinnumber));
+console.log(valueinnumber)
+Output =  33 
+
+let score = "33as"
+let valueinnumber = Number(score)
+console.log(typeof (valueinnumber));
+console.log(valueinnumber)
+Output =  NaN
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 false => 0 and vise vesa
+// ashish => true 
+// "" => false
+
+
+// ******************************* Operation *************//
+
+let value = 3
+let negvaue = -value 
+
+
