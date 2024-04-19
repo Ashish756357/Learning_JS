fa8accebe2e819f6a658b185365dceaae789d546
Ashish Raj <126260804+Ashish756357@users.noreply.github.com>
2024-04-19 20:38:00 +0530

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,10 @@
+// To print things in js
+console.log("Ashish")
+
+// TO declare a var we use let key word with var name 
+let Account_ID = 1234567
+let Account_Email = "ashish@gmail.com"
+
+// TO print all the value or var in one table we use 
+console.table([Account_ID, Account_Email])
+
