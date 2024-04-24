f3336b4da0c4bf8f5c451e2a0b3b0ac81448fdc7
Ashish Raj <126260804+Ashish756357@users.noreply.github.com>
2024-04-24 11:09:53 +0530

    Update String.js

--- String.js
+++ String.js
@@ -1,4 +1,34 @@
-const name = "Ashish"
-const repo = 100
+// const name = "Ashish"
+// const repo = 100
 
-const.log(`hello my name is ${name} and my repo count is ${repo}`};
+// // Other way to declare a string variable 
+
+// const gameName = new string('Ashish Raj')
+// console.log(gameName)
+
+// const.log(`hello my name is ${name} and my repo count is ${repo}`};
+
+// const gameName = new String('Ashish    Raj')
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toLocaleLowerCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("R"));
+// console.log(gameName.trim(0,3));
+// console.log(gameName.substring(0,4))
+// const newString = gameName.substring(5,9);
+// console.log(newString);
+
+// const newStringone = " Ashish ";
+
+// console.log(newStringone);
+// console.log(newStringone.trim( ));
+
+// const url = "https://www.youtube.com/watch?v=sscX432bMZo&t=8959s";
+
+// console.log(url.replace('?', '_'));
+// console.log(url.includes('bkl'));
+
+
